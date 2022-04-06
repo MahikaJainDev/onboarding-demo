ce36c3e50bf28079f636f8dec6378479669ab116
mahika <mahika3915@gmail.com>
2022-04-06 19:55:00 +0530

    starting with onboarding screens

--- lib/style2.dart
+++ lib/style2.dart
@@ -23,6 +23,7 @@ class _Style2State extends State<Style2> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: CarouselSlider(
@@ -39,46 +40,61 @@ class _Style2State extends State<Style2> {
                 children: [
                   Expanded(
                     child: Container(
-                      color: i.color!,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        border:const  Border(
+                          bottom: BorderSide(
+                            color: Color(0xff000000)
+                          )
+                        ),
+                        color: i.color,
+                      ),
+                      child: Image.asset('assets/images/download.jpeg',
+                        fit: BoxFit.fill,
+                      ),
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15),
                     child: Text(
                         i.heading!,
                         style: Theme.of(context).textTheme.headline3
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
                     child: Text(
                       i.text!,
                       textAlign: TextAlign.justify,
-                      style: Theme.of(context).textTheme.bodyLarge,
+                      style: Theme.of(context).textTheme.caption,
                     ),
                   ),
                 ],
               )).toList(),
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [0,1,2,3].map((e) => Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: e==index ? 20 : 10,
-                width: e==index? 20 : 10,
-                child: Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: e==index ? Colors.pink : Colors.grey,
+          SizedBox(
+            height: 200,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [0,1,2,3].map((e) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  height: 10,
+                  width: e==index? 25.0 : 10.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: e==index ? const Color(0xff000000) : Colors.grey[400],
+                    ),
                   ),
                 ),
-              ),
-            )).toList(),
+              )).toList(),
+            ),
           ),
         Padding(
-          padding: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.only(bottom: 20.0),
           child: SizedBox(
             width: 80.0,
             height: 80.0,
@@ -87,19 +103,21 @@ class _Style2State extends State<Style2> {
               children: [
                 CircularProgressIndicator(
                   value: setIndicator(index),
-                  backgroundColor: Colors.grey,
+                  backgroundColor: Colors.grey[400],
+                  strokeWidth: 2.0,
                   color: Colors.pink,
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: FloatingActionButton(
+                    elevation: 0.0,
                     backgroundColor: Colors.pink,
                     onPressed: (){
-                        controller.nextPage(duration: Duration(milliseconds: 500));
-
+                        controller.nextPage(duration: const Duration(milliseconds: 500));
                     },
-                    child: Icon(
+                    child: const Icon(
                       Icons.arrow_forward,
+                      size: 30,
                     ),
                   ),
                 )
